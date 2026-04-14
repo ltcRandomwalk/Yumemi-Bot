02ac4e552d4064328c1f41672e6b7e7c42f2ca8c
Fuuko <lwjzlw@gmail.com>
2026-04-15 04:00:06 +0800

    Add local random character photo command

--- src/plugins/photo/config.py
+++ src/plugins/photo/config.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
-from src.utils.paths import IMAGE_RESOURCE_DIR
+from src.utils.paths import CHARACTER_DATA_PATH, IMAGE_RESOURCE_DIR
+
 
 class PluginConfig(BaseModel):
     image_base_folder: str = str(IMAGE_RESOURCE_DIR)
+    character_json_path: str = str(CHARACTER_DATA_PATH)
+    photo_superusers: list[str] = []
